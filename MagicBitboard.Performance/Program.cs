b9ccbf3405d55f2d9119c2feabb13020dabb73f3
Valdemar Erlingsson <valdiorn@gmail.com>
2012-09-18 01:38:01 +0000

    Started working on magic bitboards

---
+++ MagicBitboard.Performance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicBitboard.Performance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int idx = 0; idx < 64; idx++)
+			{
+				//int idx = 0;
+
+				var perms = Bitboard.GetRookPermutations(idx);
+				var map = new Dictionary<ulong, ulong>();
+
+				foreach (var perm in perms)
+				{
+					map[perm] = Bitboard.GetRookMoves(perm, idx);
+				}
+
+				int count = map.Select(x => x.Value).Distinct().Count();
+
+				// --------------------------------
+				int bits = 14; // 2^14
+				Console.WriteLine("Testing at " + bits + " bits = " + (Math.Pow(2, bits) / 1024) + " kilobytes per position");
+				var start = DateTime.Now;
+
+				Bitboard.FindMagic(map, bits);
+
+				Console.WriteLine("Time: " + (DateTime.Now - start).TotalMilliseconds + " ms");
+
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ MagicBitboard/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicBitboard
+{
+	public class Board
+	{
+		public UInt64 Occupancy;
+
+		/// <summary>
+		/// Get the X coordinate of the tile. 0...7
+		/// </summary>
+		/// <param name="square"></param>
+		/// <returns></returns>
+		public static int X(int tile)
+		{
+			// 128 constant prevent negative numbers in modulu output
+			return (128 + tile) % 8;
+		}
+
+		/// <summary>
+		/// Get the Y coordinate of the tile. 0...7
+		/// </summary>
+		/// <param name="square"></param>
+		/// <returns></returns>
+		public static int Y(int tile)
+		{
+			return tile >> 3;
+		}
+	}
+}
